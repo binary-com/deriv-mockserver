d41aedd1811d905b3b08823a96207a9894801296
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-07-28 11:38:59 +0800

    fix: address comments

--- src/api/topup-virtual.api.ts
+++ src/api/topup-virtual.api.ts
@@ -1,28 +1,19 @@
 import { InterceptedAPIHandler } from '../types/base.type';
 import { TopUpVirtualRequest } from '../types/topup-virtual.type';
+import { handleGenericError } from '../utils/error.utils';
 
 export const topupVirtual = ({ data, ws, session }: InterceptedAPIHandler) => {
     const { topup_virtual, req_id } = data as TopUpVirtualRequest;
     const virtual_account = session.accounts.find(
         account => account.is_virtual === 1 && account.landing_company_name === 'virtual'
     );
+    if (!virtual_account) {
+        return handleGenericError('InvalidAccount', 'Virtual account not found', ws, data);
+    }
 
     const virtual_balance = virtual_account?.balance;
 
     const amount = 10000 - virtual_balance!;
-
-    if (!virtual_account) {
-        return ws.send(
-            JSON.stringify({
-                error: {
-                    code: 'VirtualAccountNotFound',
-                    message: 'No virtual account found.',
-                },
-                msg_type: 'topup_virtual',
-                req_id,
-            })
-        );
-    }
 
     const response = {
         echo_req: { req_id, topup_virtual },
